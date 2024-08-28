cc9c2aa22389d043242ee8cdf98f713b28219856
Richard Rivera <richard.rivera.roman@gmail.com>
2024-08-28 18:22:18 -0400

    Created a custom hook to fetch the games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,42 +1,13 @@
-import { CanceledError } from 'axios';
-import apiClient from '../services/apiClient';
-import { createEffect, createSignal, For } from 'solid-js';
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface fetchGamesResponse {
-  count: number;
-  results: Game[];
-}
+import { For } from 'solid-js';
+import useGames from '../hooks/useGames';
 
 function GameGrid() {
-  const [games, setGames] = createSignal<Game[]>([]);
-  const [error, setError] = createSignal('');
-  const [isLoading, setLoading] = createSignal(false);
-
-  createEffect(() => {
-    const controller = new AbortController();
-
-    setLoading(true);
-    apiClient
-      .get<fetchGamesResponse>('/gamesx', { signal: controller.signal })
-      .then((res) => setGames(res.data.results))
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-      })
-      .finally(() => setLoading(false));
-
-    return () => controller.abort();
-  }, [games()]);
+  const { games, error, isLoading } = useGames();
 
   return (
     <>
-      {error() && <p>{error()}</p>}
       {isLoading() && <p>Loading...</p>}
+      {error() && <p>{error()}</p>}
       <ul>
         <For each={games()}>{(game) => <li>{game.name}</li>}</For>
       </ul>
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,39 @@
+import { createEffect, createSignal } from 'solid-js';
+import apiClient from '../services/apiClient';
+import { CanceledError } from 'axios';
+
+type Game = {
+  id: number;
+  name: string;
+};
+
+type FetchGamesResponse = {
+  count: number;
+  results: Game[];
+};
+
+function useGames() {
+  const [games, setGames] = createSignal<Game[]>([]);
+  const [error, setError] = createSignal('');
+  const [isLoading, setLoading] = createSignal(false);
+
+  createEffect(() => {
+    const controller = new AbortController();
+
+    setLoading(true);
+    apiClient
+      .get<FetchGamesResponse>('/games', { signal: controller.signal })
+      .then((res) => setGames(res.data.results))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      })
+      .finally(() => setLoading(false));
+
+    return () => controller.abort();
+  }, [games()]);
+
+  return { games, error, isLoading };
+}
+
+export default useGames;
