0d913a9d9ef673f78219c3d61caef0e676617b16
Richard Rivera <richard.rivera.roman@gmail.com>
2024-08-27 20:09:50 -0400

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import ThemeSwitcher from './components/ThemeSwitch';
+import './App.scss';
 
 function App() {
   return (
-    <div>
-      <ThemeSwitcher />
+    <div class="app-container bg-zinc-100 dark:bg-zinc-900">
+      <nav class="app-nav">
+        <h2 class="bg-purple-500">Nav</h2>
+        <ThemeSwitcher />
+      </nav>
+      <aside class="app-aside">
+        <h2 class="bg-blue-500">Aside</h2>
+      </aside>
+      <main class="grid-main mx-auto">
+        <div class="flex gap-2">
+          <h2 class="bg-orange-500">Main</h2>
+        </div>
+      </main>
     </div>
   );
 }
