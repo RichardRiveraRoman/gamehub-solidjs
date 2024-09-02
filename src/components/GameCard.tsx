99f3a548d2512848505ef99318a62e7b87dbf2c9
Richard Rivera <richard.rivera.roman@gmail.com>
2024-09-02 13:51:44 -0400

    Display game rating

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,4 +1,6 @@
 import type { Game } from '../hooks/useGames';
+import getCroppedImageUrl from '../services/image-url';
+import GameRating from './GameRating';
 import GameScore from './GameScore';
 import PlatformIcons from './PlatformIcons';
 
@@ -11,8 +13,7 @@ function GameCard({ game }: Props) {
     <div class="max-w-64 max-h-96 rounded-lg shadow-lg overflow-hidden custom-bg outline outline-slate-400/50 outline-1 dark:outline-none transition-colors duration-200 flex flex-col">
       <img
         class="w-64 h-48 object-cover"
-        // src={getCroppedImageUrl(game.background_image)}
-        src={game.background_image}
+        src={getCroppedImageUrl(game.background_image)}
         alt={game.name}
       />
       <div class="px-6 py-4 flex flex-col flex-grow">
@@ -28,7 +29,7 @@ function GameCard({ game }: Props) {
             )}
             {game.metacritic && <GameScore score={game.metacritic} />}
           </div>
-          {/* <GameRating rating={game.rating_top} /> */}
+          <GameRating rating={game.rating_top} />
         </div>
       </div>
     </div>
---
+++ src/components/GameRating.tsx
@@ -0,0 +1,32 @@
+const GameRating = ({ rating }: { rating: number }) => {
+  const mapRating = (rating: number): string => {
+    const ratings: { [key: number]: string } = {
+      1: 'Skip',
+      3: 'Meh',
+      4: 'Recommended',
+      5: 'Exceptional',
+    };
+    return ratings[rating] || 'Skip';
+  };
+
+  const ratingString = mapRating(rating);
+
+  const colors: { [key: string]: string } = {
+    Exceptional: 'bg-green-500 dark:bg-green-600',
+    Recommended: 'bg-blue-500 dark:bg-blue-600',
+    Meh: 'bg-yellow-500 dark:bg-yellow-600',
+    Skip: 'bg-red-500 dark:bg-red-600',
+  };
+
+  return (
+    <span
+      class={`inline-block rounded-full px-3 py-1 text-sm font-semibold text-white ${
+        colors[ratingString] || 'bg-gray-500 dark:bg-gray-600'
+      }`}
+    >
+      {ratingString}
+    </span>
+  );
+};
+
+export default GameRating;
