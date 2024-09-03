d1a5a25eaaa770747624cb42d1d62eb7b962df59
Richard Rivera <richard.rivera.roman@gmail.com>
2024-09-03 06:24:53 -0400

    Added images to genres

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,33 @@
+import { For } from 'solid-js';
+import useGenres, { type Genre } from '../hooks/useGenres';
+import getCroppedImageUrl from '../services/image-url';
+import Spinner from './Spinner';
+
+function GenreList() {
+  const { data, error, isLoading } = useGenres();
+
+  if (error()) return null;
+
+  if (isLoading()) return <Spinner />;
+
+  return (
+    <ul>
+      <For each={data()}>
+        {(genre) => (
+          <li>
+            <div class="m-2 flex items-center gap-2 px-2 py-1 text-black dark:text-white">
+              <img
+                class="size-9 rounded-xl object-cover"
+                src={getCroppedImageUrl(genre.image_background)}
+                alt={genre.name}
+              />
+              <p>{genre.name}</p>
+            </div>
+          </li>
+        )}
+      </For>
+    </ul>
+  );
+}
+
+export default GenreList;
