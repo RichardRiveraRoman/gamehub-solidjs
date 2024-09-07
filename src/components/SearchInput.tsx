b5fa23e8516971267a9ba433598311326f86061a
Richard Rivera <richard.rivera.roman@gmail.com>
2024-09-07 19:25:47 -0400

    Fixed type definition

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,4 +1,4 @@
-import search from '../assets/Icons/search.png'
+import search from '../assets/Icons/search.png';
 import { tw } from 'typewind';
 
 const SearchInput = ({
@@ -6,7 +6,7 @@ const SearchInput = ({
 }: {
   onSearch: (searchText: string) => void;
 }) => {
-  let ref: HTMLInputElement | null;
+  let ref: HTMLInputElement | undefined;
 
   return (
     <form
@@ -16,13 +16,25 @@ const SearchInput = ({
         if (ref) onSearch(ref.value);
       }}
     >
-      <img src={search} alt="Search Icon"
-        class={tw.relative.inset_x_7.size_5.raw('platform-icon').dark(tw.text_gray_300).text_lg}
+      <img
+        src={search}
+        alt="Search Icon"
+        class={
+          tw.relative.inset_x_7.size_5
+            .raw('platform-icon')
+            .dark(tw.text_gray_300).text_lg
+        }
       />
       <input
-        ref={(el) => (ref = el)}
+        ref={ref}
         type="search"
-        class={tw.border_0.h_8.px_8.dark(tw.caret_white.bg_zinc_700).bg_slate_300.outline_zinc_500.w_full.outline.outline_1.rounded_full.placeholder(tw.text_gray_500).focus(tw.ring_0).sm(tw.text_sm)}
+        class={tw.border_0.h_8.px_8
+          .dark(tw.caret_white.bg_zinc_700)
+          .bg_slate_300.outline_zinc_500.w_full.outline.outline_1.rounded_full.placeholder(
+            tw.text_gray_500,
+          )
+          .focus(tw.ring_0)
+          .sm(tw.text_sm)}
         placeholder="Search"
       />
     </form>
