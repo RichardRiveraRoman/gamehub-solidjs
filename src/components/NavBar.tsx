ecffab47e5f06149c03da9355b150d31c4683c04
Richard Rivera <richard.rivera.roman@gmail.com>
2024-09-13 14:16:24 -0400

    Changed to inline property definition

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,25 +1,25 @@
-import type { JSX } from 'solid-js/jsx-runtime';
 import logo from '../assets/Logo/logo.webp';
 import ThemeSwitch from './ThemeSwitch';
 import SearchInput from './SearchInput';
 import { tw } from 'typewind';
 
-interface Props extends JSX.HTMLAttributes<HTMLElement> {
+function NavBar({
+  onSearch,
+  class: className,
+}: {
   onSearch: (searchText: string) => void;
-}
-
-function NavBar(props: Props) {
-  // function NavBar(props: JSX.HTMLAttributes<HTMLElement>, {onSearch}: {onSearch: (searchText: string) => void} ) {
+  class?: string;
+}) {
   return (
     <nav
-      class={`${tw.flex.w_full.items_center.gap_6.px_2.text_black.dark(tw.text_white)} ${props.class}`}
+      class={`${tw.flex.w_full.items_center.gap_6.px_2.text_black.dark(tw.text_white)} ${className}`}
     >
       <img
         src={logo}
         alt="A rounded square logo, with a white background showing 6 droplets connect in center. The logo contains a hex nut with a circle in the center of the nut. The logo represents connectivity. All the elements that make up the logo besides the background are somewhat of a purple hue."
         class={tw.size_20}
       />
-      <SearchInput onSearch={props.onSearch} />
+      <SearchInput onSearch={onSearch} />
       <ThemeSwitch />
     </nav>
   );
